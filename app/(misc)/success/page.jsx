cfb104bb68093b7544c15872134e1934c7ed09fa
Ali IMran <si07886@st.habib.edu.pk>
2025-07-17 07:43:53 -0500

    success page integration

--- app/(misc)/success/page.jsx
+++ app/(misc)/success/page.jsx
@@ -1,46 +1,36 @@
 'use client'
-
 import Link from 'next/link.js';
 import { useSearchParams } from 'next/navigation'
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-
 export const dynamic = 'force-dynamic'
 
 const SuccessPage = () => {
-  const dispatch = useDispatch();
-
-  const getOrderDetails = async (sessionId) => {
-    try {
-      const url = `/api/getSession?sessionId=${sessionId}`
-      const res = await fetch(url);
-      
-      if (!res.ok) {
-        toast.error('Error getting order details. Although, your order has been placed. Contact support for more info.');
-        return;
-      }
-    
-      const data = await res.json();
-    } catch (err) {
-      toast.error(err.message);
-    } finally {
-    //   dispatch(emptyCart());
-    }
-  };
-
-  const params = useSearchParams()
-  const sessionId = params.get('sessionId')
-
-  getOrderDetails(sessionId)
-
-
-  return (
-    <div className='w-full min-h-[50vh] flex items-center justify-center flex-col'>
-      <h2>Congratulations, Your order has been placed!</h2>
-      <Link href='/ejuice'>Continue Chopping</Link>
-    </div>
-  )
+const dispatch = useDispatch();
+const getOrderDetails = async (sessionId) => {
+try {
+const url = `/api/getSession?sessionId=${sessionId}`
+const res = await fetch(url);
+if (!res.ok) {
+toast.error('Kesalahan mendapatkan detail pesanan. Meskipun demikian, pesanan Anda telah ditempatkan. Hubungi dukungan untuk informasi lebih lanjut.');
+return;
+ }
+const data = await res.json();
+ } catch (err) {
+toast.error(err.message);
+ } finally {
+// dispatch(kosongkanKeranjang());
+ }
+ };
+const params = useSearchParams()
+const sessionId = params.get('sessionId')
+getOrderDetails(sessionId)
+return (
+<div className='w-full min-h-[50vh] flex items-center justify-center flex-col'>
+<h2>Selamat, pesanan Anda telah berhasil ditempatkan!</h2>
+<Link href='/ejuice'>Lanjut Berbelanja</Link>
+</div>
+ )
 }
-
 export default SuccessPage

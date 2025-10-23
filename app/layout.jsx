870486d59f50f039ac561227ff082c4a97ea171f
Ali IMran <si07886@st.habib.edu.pk>
2025-10-23 10:09:19 -0500

    Google tags added

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,9 +31,34 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={avenirFont.className}>
       <head>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" /></head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        
+        {/* Google Tag Manager */}
+        <Script 
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NZVHDSM7');
+            `
+          }}
+        />
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NZVHDSM7"
+            height="0" 
+            width="0" 
+            style={{display:'none', visibility:'hidden'}}
+          ></iframe>
+        </noscript>
+
         <ReduxProvider>
           <Layout>{children}</Layout>
         </ReduxProvider>
